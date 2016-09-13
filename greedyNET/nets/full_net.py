e90892edb71aa89cb0c32e7fe3128257fc8a80ea
Alberto Bailoni <abailoni@ialgpu02.iwr.uni-heidelberg.de>
2016-09-13 11:16:12 +0200

    Added full backprop trained network

--- greedyNET/nets/full_net.py
+++ greedyNET/nets/full_net.py
@@ -1,129 +1,111 @@
-import time
 import numpy as np
 from copy import deepcopy
 import json
 
-from lasagne.init import HeNormal
+from lasagne.init import HeNormal, Normal
 import theano.tensor as T
 from lasagne import layers
-from lasagne.layers import set_all_param_values, get_all_param_values
-from lasagne.nonlinearities import rectify, identity
-import lasagne.init
+from lasagne.nonlinearities import rectify
 
-from lasagne.init import HeNormal
 
-import greedyNET.greedy_utils as greedy_utils
+import pretr_nets.vgg16 as vgg16
+
+
+import mod_nolearn.utils as utils
+from mod_nolearn.nets.modNeuralNet import BatchIterator_mod
 import mod_nolearn.nets.segmNet as segmNet
-from mod_nolearn.segmentFcts import pixel_accuracy_sigmoid
+import mod_nolearn.segmentFcts as segmentFcts
 
 
 class fullNet(object):
-    def __init__(self,previous_layers,input_filters,**kwargs):
+    def __init__(self,num_VGG16_layers,num_conv_layers,**kwargs):
         info = deepcopy(kwargs)
         # -----------------
         # General attributes:
         # -----------------
-        self.filter_size1 = kwargs.pop('filter_size1', 7)
-        self.filter_size2 = kwargs.pop('filter_size2', 7)
-        self.num_filters1 = kwargs.pop('num_filters1', 5)
-        self.input_filters = input_filters
-        self.previous_layers = previous_layers
-        self.num_classes = 1
+        self.filter_size = kwargs.pop('filter_size', 3)
+        self.num_filters = kwargs.pop('num_filters', 65)
+        self.num_classes = kwargs.pop('num_classes', 2)
+        self.num_conv_layers = kwargs.pop('num_conv_layers', 5)
         self.xy_input = kwargs.pop('xy_input', (None, None))
-        self.eval_size = kwargs.pop('eval_size', 0.1)
-        # Checks:
-        if "num_classes" in kwargs:
-            raise ValueError('Multy-class classification boosting not implemented for the moment. "num_classes" parameter is fixed to 2')
-        if "train_split" in kwargs:
-            raise ValueError('The option train_split is not used. Use eval_size instead.')
+        self.eval_size = kwargs.pop('eval_size', 0.2)
+
+        self.model_name = kwargs.pop('model_name', 'greedyNET')
+        self.BASE_PATH_LOG = kwargs.pop('BASE_PATH_LOG', "./logs/")
+        self.BASE_PATH_LOG_MODEL = self.BASE_PATH_LOG+self.model_name+'/'
+
 
 
         # -------------------------------------
         # Specific parameters:
         # -------------------------------------
         # self.weights_HeGainin = kwargs.pop('weights_HeGain', 1.)
-        kwargs.setdefault('name', 'convSoftmax')
-        self.num_nodes = kwargs.pop('num_nodes', 5)
-        self.batch_size = kwargs.pop('batch_size', 100)
+        kwargs.setdefault('name', 'fullNet')
+        self.init_weight = kwargs.pop('init_weight', 1e-3)
+        self.batch_size = kwargs.pop('batch_size', 20)
         self.batchShuffle = kwargs.pop('batchShuffle', True)
         self.active_nodes = 0
 
-        customBatchIterator = greedy_utils.BatchIterator_Greedy(
+        customBatchIterator = BatchIterator_mod(
             batch_size=self.batch_size,
             shuffle=self.batchShuffle,
-            previous_layers=self.previous_layers
         )
 
         # -------------------------------------
         # CONSTRUCT NETWORK:
         # -------------------------------------
-        netLayers = [
-            # layer dealing with the input data
-            (layers.InputLayer, {
-                'name': 'inputLayer',
-                'shape': (None, self.input_filters, self.xy_input[0], self.xy_input[1])}),
+        self.num_VGG16_layers = int(num_VGG16_layers)
+        self.layers = vgg16.nolearn_vgg16_layers(data_size=(None, 3, self.xy_input[0], self.xy_input[1]))[:self.num_VGG16_layers+1]
+        self.layers += [
             (layers.Conv2DLayer, {
-                'name': 'conv1',
-                'num_filters': self.num_nodes*self.num_filters1*self.num_classes,
-                'filter_size': self.filter_size1,
-                'W': HeNormal(np.sqrt(2)),
+                'name': 'conv%d' %i,
+                'num_filters': self.num_filters,
+                'filter_size': self.filter_size,
+                'W': Normal(std=self.init_weight),
                 'pad':'same',
-                'nonlinearity': rectify}),
-            (layers.Conv2DLayer, {
-                'name': 'conv2',
-                'num_filters': self.num_nodes*self.num_filters1*self.num_classes,
-                'filter_size': self.filter_size1,
-                'W': HeNormal(np.sqrt(2)),
-                'pad':'same',
-                'nonlinearity': rectify}),
-            (layers.Conv2DLayer, {
-                'name': 'conv3',
-                'num_filters': self.num_nodes*self.num_filters1*self.num_classes,
-                'filter_size': self.filter_size1,
-                'W': HeNormal(np.sqrt(2)),
-                'pad':'same',
-                'nonlinearity': rectify}),
-            (layers.Conv2DLayer, {
-                'name': 'conv3',
-                'num_filters': self.num_nodes*self.num_filters1*self.num_classes,
-                'filter_size': self.filter_size1,
-                'W': HeNormal(np.sqrt(2)),
-                'pad':'same',
-                'nonlinearity': rectify}),
+                'nonlinearity': rectify}) for i in range(self.num_conv_layers)] + [
             (layers.Conv2DLayer, {
                 'name': 'conv_last',
                 'num_filters': self.num_classes,
-                'filter_size': self.filter_size2,
-                'W': HeNormal(np.sqrt(2)),
+                'filter_size': self.filter_size,
+                'W': Normal(std=self.init_weight),
                 'pad':'same',
-                'nonlinearity': segmNet.sigmoid_segm}),
+                'nonlinearity': segmNet.softmax_segm}),
         ]
 
         self.net = segmNet.segmNeuralNet(
-            layers=netLayers,
+            layers=self.layers,
             batch_iterator_train = customBatchIterator,
             batch_iterator_test = customBatchIterator,
-            objective_loss_function = segmNet.binary_crossentropy_segm,
-            scores_train = [('trn pixelAcc', pixel_accuracy_sigmoid)],
-            scores_valid = [('val pixelAcc', pixel_accuracy_sigmoid)],
-            y_tensor_type = T.ftensor3,
+            objective_loss_function = segmNet.categorical_crossentropy_segm,
+            scores_train = [('trn pixelAcc', segmentFcts.pixel_accuracy)],
+            # scores_valid = [('val pixelAcc', pixel_accuracy_sigmoid)],
+            y_tensor_type = T.ltensor3,
             eval_size=self.eval_size,
-            regression = True,
+            regression = False,
+            logs_path= self.BASE_PATH_LOG_MODEL,
             **kwargs
         )
 
+
         self.net.initialize()
+
+        # --------------------
+        # Copy vgg16 weights:
+        # --------------------
+        self.net = vgg16.nolearn_insert_weights_vgg16(self.net,self.num_VGG16_layers)
 
 
         # -------------------------------------
         # SAVE INFO NET:
         # -------------------------------------
-        info['num_classes'] = 1
+        info['num_classes'] = self.num_classes
         info.pop('update', None)
         info.pop('on_epoch_finished', None)
         info.pop('on_batch_finished', None)
         info.pop('on_training_finished', None)
         for key in [key for key in info if 'update_' in key]:
             info[key] = info[key].get_value().item()
-        json.dump(info, file(info['logs_path']+'/info-net.txt', 'w'))
+        utils.create_dir(self.BASE_PATH_LOG_MODEL)
+        json.dump(info, file(self.BASE_PATH_LOG_MODEL+'info-net.txt', 'w'))
 
